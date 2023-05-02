2e280400c13078d62658e952aa6b296373030c6e
Kim SungWoo <gokorea1214@kakao.com>
2023-05-02 11:20:48 +0900

    serializer create and manage.py Plus and requirements create

---
+++ users/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import serializers
+from users.models import MyUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        password = user.password
+        user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        user = super().update(instance, validated_data)
+        password = user.password
+        user.set_password(password)
+        user.save()
+        return user
